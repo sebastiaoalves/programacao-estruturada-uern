30afc3966d611b2e7f3a9f04f492d39bf6ba0472
Sebastiao Alves <sebastiao.alves@gmail.com>
2020-12-02 10:55:57 -0300

    Adicionando um projeto Qt.

---
+++ projeto-pife/Pif-paf/telaprincipal.h
@@ -0,0 +1,21 @@
+#ifndef TELAPRINCIPAL_H
+#define TELAPRINCIPAL_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class TelaPrincipal; }
+QT_END_NAMESPACE
+
+class TelaPrincipal : public QWidget
+{
+    Q_OBJECT
+
+public:
+    TelaPrincipal(QWidget *parent = nullptr);
+    ~TelaPrincipal();
+
+private:
+    Ui::TelaPrincipal *ui;
+};
+#endif // TELAPRINCIPAL_H
